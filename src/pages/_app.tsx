116bd7e5e589fada6f977361332df61af9d2743b
Danny Lin <75951607+dannynotsmart@users.noreply.github.com>
2024-08-11 22:50:42 -0400

    base functionality done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet='UTF-8'/>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
+        <link rel="icon" href="/assets/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
